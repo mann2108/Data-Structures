37d796d1c35b9e722d15c0e2646660cee67c8394
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-01-16 17:15:33 +0530

    Create SegmentTree-8.cpp

---
+++ Segment Tree/SegmentTree-8.cpp
@@ -0,0 +1,19 @@
+#inlclude<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main(){
+    ll n,q,k;
+    cin>>n>>q>>k;
+    ll a[n];
+    for(ll i=0;i<n;i++)cin>>a[i];
+    while(q--){
+        ll choice,l,r;
+        cin>>choice>>l>>r;
+        if(choice==1){
+            ll val;
+        }
+        else{
+            ll rem;
+        }
+    }
+}
